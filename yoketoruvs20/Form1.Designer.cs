1bd5b255399f192031788839b9efcf6caeae90db
ST0011 <st.game.program@gmail.com>
2020-09-10 11:47:41 +0900

    タイトル作成

--- yoketoruvs20/Form1.Designer.cs
+++ yoketoruvs20/Form1.Designer.cs
@@ -28,13 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartButtom = new System.Windows.Forms.Button();
+            this.copyrightlabel = new System.Windows.Forms.Label();
+            this.timelabel = new System.Windows.Forms.Label();
+            this.leftlabel = new System.Windows.Forms.Label();
+            this.highscorelabel = new System.Windows.Forms.Label();
+            this.GAMEOVERlabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clearlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Adobe Gothic Std B", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(175, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(450, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "よけとる V２０２０";
+            // 
+            // StartButtom
+            // 
+            this.StartButtom.Font = new System.Drawing.Font("HGPｺﾞｼｯｸE", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.StartButtom.Location = new System.Drawing.Point(295, 355);
+            this.StartButtom.Name = "StartButtom";
+            this.StartButtom.Size = new System.Drawing.Size(211, 56);
+            this.StartButtom.TabIndex = 1;
+            this.StartButtom.Text = "START";
+            this.StartButtom.UseVisualStyleBackColor = true;
+            // 
+            // copyrightlabel
+            // 
+            this.copyrightlabel.AutoSize = true;
+            this.copyrightlabel.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.copyrightlabel.Location = new System.Drawing.Point(491, 414);
+            this.copyrightlabel.Name = "copyrightlabel";
+            this.copyrightlabel.Size = new System.Drawing.Size(297, 27);
+            this.copyrightlabel.TabIndex = 2;
+            this.copyrightlabel.Text = "Copyright © 鈴木　智也";
+            // 
+            // timelabel
+            // 
+            this.timelabel.AutoSize = true;
+            this.timelabel.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.timelabel.Location = new System.Drawing.Point(12, 9);
+            this.timelabel.Name = "timelabel";
+            this.timelabel.Size = new System.Drawing.Size(219, 48);
+            this.timelabel.TabIndex = 3;
+            this.timelabel.Text = "TIME：100";
+            // 
+            // leftlabel
+            // 
+            this.leftlabel.AutoSize = true;
+            this.leftlabel.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.leftlabel.Location = new System.Drawing.Point(665, 9);
+            this.leftlabel.Name = "leftlabel";
+            this.leftlabel.Size = new System.Drawing.Size(123, 37);
+            this.leftlabel.TabIndex = 4;
+            this.leftlabel.Text = "★：１０";
+            // 
+            // highscorelabel
+            // 
+            this.highscorelabel.AutoSize = true;
+            this.highscorelabel.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.highscorelabel.Location = new System.Drawing.Point(276, 221);
+            this.highscorelabel.Name = "highscorelabel";
+            this.highscorelabel.Size = new System.Drawing.Size(248, 35);
+            this.highscorelabel.TabIndex = 5;
+            this.highscorelabel.Text = "HighScore 1000";
+            // 
+            // GAMEOVERlabel
+            // 
+            this.GAMEOVERlabel.AutoSize = true;
+            this.GAMEOVERlabel.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.GAMEOVERlabel.Location = new System.Drawing.Point(259, 133);
+            this.GAMEOVERlabel.Name = "GAMEOVERlabel";
+            this.GAMEOVERlabel.Size = new System.Drawing.Size(282, 48);
+            this.GAMEOVERlabel.TabIndex = 6;
+            this.GAMEOVERlabel.Text = "GAME OVER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(329, 365);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 37);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "タイトルへ";
+            // 
+            // clearlabel
+            // 
+            this.clearlabel.AutoSize = true;
+            this.clearlabel.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.clearlabel.Location = new System.Drawing.Point(328, 133);
+            this.clearlabel.Name = "clearlabel";
+            this.clearlabel.Size = new System.Drawing.Size(165, 48);
+            this.clearlabel.TabIndex = 8;
+            this.clearlabel.Text = "CLEAR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clearlabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.GAMEOVERlabel);
+            this.Controls.Add(this.highscorelabel);
+            this.Controls.Add(this.leftlabel);
+            this.Controls.Add(this.timelabel);
+            this.Controls.Add(this.copyrightlabel);
+            this.Controls.Add(this.StartButtom);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartButtom;
+        private System.Windows.Forms.Label copyrightlabel;
+        private System.Windows.Forms.Label timelabel;
+        private System.Windows.Forms.Label leftlabel;
+        private System.Windows.Forms.Label highscorelabel;
+        private System.Windows.Forms.Label GAMEOVERlabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label clearlabel;
     }
 }
 
